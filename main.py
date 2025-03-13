6b35f5da0164f7f7709dd70a20901834f1fc7fea
j-willy <jake.willy@pm.me>
2025-03-12 21:46:16 -0400

    testing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_letters
+from stats import count_words, count_letters, sorting_fucntion
 
 def main():
     book_path = "books/frankenstein.txt"
@@ -6,9 +6,13 @@ def main():
     word_count = count_words(book)
     char_dict = count_letters(book)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"{word_count} words found in the document")
+    print("--------- Character Count -------")
     print(char_dict)
-    
+    print("============= END ===============")
     
 def get_book_text(filepath):
    with open(filepath) as file:
